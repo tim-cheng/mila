b57395a5b1c06ade5f617cfc17d30b6610e1fe43
Tim Cheng <chengyuqing@gmail.com>
2014-05-03 14:12:05 -0700

    more unit tests

--- server/database.go
+++ server/database.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/coopernurse/gorp"
 	_ "github.com/lib/pq"
 	"log"
@@ -12,21 +11,6 @@ type MyDb struct {
 	gorp.DbMap
 }
 
-func (db *MyDb) addTestData() {
-	// delete existing entries
-	//  err := dbmap.TruncateTables()
-	//  checkErr(err, "Truncate failed")
-
-	// insert some seed data
-	u1, _ := db.newUser("email", "a@b.c", "abcd1234", "Boo", "Daa")
-	u2, _ := db.newUser("email", "b@c.d", "1234abcd", "Nee", "Naa")
-
-	err := db.Insert(u1, u2)
-	if err != nil {
-		fmt.Println("failed to insert: ", err)
-	}
-}
-
 func NewDb() *MyDb {
 	db, err := sql.Open("postgres", "user=timcheng dbname=mila_dev sslmode=disable")
 	checkErr(err, "sql.Open failed")
@@ -34,7 +18,6 @@ func NewDb() *MyDb {
 	dbmap.AddTableWithName(User{}, "users").SetKeys(true, "Id")
 	dbmap.AddTableWithName(Connection{}, "connections").SetKeys(false, "user1_id", "user2_id")
 	dbmap.AddTableWithName(Post{}, "posts").SetKeys(true, "Id")
-	dbmap.addTestData()
 	return dbmap
 }
 
--- server/server_test.go
+++ server/server_test.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bytes"
-	"fmt"
-	"io/ioutil"
+	//"fmt"
+	//"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -11,19 +11,47 @@ import (
 	"time"
 )
 
-func testClient(email, password, method, path, params string) {
+func testClient(email, password, method, path, params string) int {
 	client := &http.Client{}
 	req, err := http.NewRequest(method, "http://localhost:8080"+path, bytes.NewBufferString(params))
-	req.SetBasicAuth(email, password)
+	if err != nil {
+		return 0
+	}
+	if email != "" {
+		req.SetBasicAuth(email, password)
+	}
 	if method == "POST" {
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 		req.Header.Add("Content-Length", strconv.Itoa(len(params)))
 	}
 	resp, err := client.Do(req)
-	fmt.Printf("req: %s\nresp: %v\nerr: %v\n", path, resp, err)
-	contents, err := ioutil.ReadAll(resp.Body)
-	if err == nil {
-		fmt.Println(string(contents))
+	if err != nil {
+		return 0
+	}
+	//fmt.Printf("req: %s\nresp: %v\nerr: %v\n", path, resp, err)
+	//contents, err := ioutil.ReadAll(resp.Body)
+	//if err == nil {
+	//  fmt.Println(string(contents))
+	//}
+
+	return resp.StatusCode
+}
+
+func createUser(email, password, firstname, lastname string) int {
+	params := url.Values{}
+	params.Add("email", email)
+	params.Add("password", password)
+	params.Add("first_name", firstname)
+	params.Add("last_name", lastname)
+	return testClient("", "", "POST", "/users", params.Encode())
+}
+
+func checkCode(t *testing.T, msg string, code int, expect int) {
+	t.Log("test: " + msg)
+	if code == expect {
+		t.Log("passed!")
+	} else {
+		t.Errorf("failed: expect %d, got %d\n", expect, code)
 	}
 }
 
@@ -31,15 +59,33 @@ func TestBasic(t *testing.T) {
 	go startServer()
 	time.Sleep(time.Second * 2)
 
-	e, p := "a@b.c", "abcd1234"
+	//	testClient(e, p, "GET", "/users/1", "")
 
-	testClient(e, p, "GET", "/users/1", "")
+	var code int
+	// create users
+	numList := []string{"1", "2", "3", "4", "5", "6", "7", "8"}
+	for _, v := range numList {
+		e, p, f, l := "user"+v+"@test.com", "testtest"+v, "First"+v, "Last"+v
+		code = createUser(e, p, f, l)
+		checkCode(t, "create user", code, 201)
+	}
 
-	params := url.Values{}
-	params.Add("email", "test@test.com")
-	params.Add("password", "234!G)...")
-	params.Add("first_name", "fslfs")
-	params.Add("last_name", "fasfsf")
-	testClient(e, p, "POST", "/users", params.Encode())
+	// create user with same username:
+	code = createUser("user4@test.com", "sdfsfss", "sfsfs", "sfsfs")
+	checkCode(t, "create user with same username", code, 404)
 
+	// get user
+	e, p := "user1@test.com", "testtest1"
+	code = testClient(e, p, "GET", "/users/1", "")
+	checkCode(t, "get user info self", code, 200)
+	code = testClient(e, p, "GET", "/users/2", "")
+	checkCode(t, "get user info others", code, 200)
+
+	// user doesn't exist
+	code = testClient(e, p, "GET", "/user/100", "")
+	checkCode(t, "get user info doesn't exist", code, 404)
+
+	// auth fail
+	testClient(e, "testtest", "GET", "/user/1", "")
+	checkCode(t, "get user info auth fail", code, 404)
 }
