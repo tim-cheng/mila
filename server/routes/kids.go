ce74676da5e92eb92b8cfa372493dd0693157b55
Tim Cheng <chengyuqing@gmail.com>
2014-05-25 08:27:42 -0700

    fix kid name

--- server/routes/kids.go
+++ server/routes/kids.go
@@ -40,7 +40,7 @@ func (rt *Routes) GetKids(params martini.Params, r render.Render) {
 		retKids := make([]map[string]interface{}, len(kids))
 		for i := range kids {
 			retKids[i] = map[string]interface{}{
-				"name": kids[i].ParentId,
+				"name": kids[i].Name,
 				"age":  birthdayToAge(kids[i].Birthday),
 				"boy":  kids[i].IsBoy,
 			}
